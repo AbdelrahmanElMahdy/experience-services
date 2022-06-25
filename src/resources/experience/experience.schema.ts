cd86b1a7585ee396619ad0c5103ca39fbd8872c9
mahdy <a.b.mahdey@gmail.com>
2022-06-25 15:59:07 +0200

    extending schema
    adding more mution and query to the schema and move the candidates input

--- src/resources/experience/experience.schema.ts
+++ src/resources/experience/experience.schema.ts
@@ -1,25 +1,41 @@
 export default `
-type candidatesExperience{
+type candidateExperience{
     id: Int!
     candidate_id: Int!
     company_name: String!
     role:String!
 }
 
+
 type CandidateTags{
     id: Int!
     candidate_id: Int!
     tag_name:String!
 }
-type candidatesExperienceWithTags{
-    tags:[String]
-    companies:[candidatesExperience]
 
+type candidateExperienceWithTags{
+    tags:[String]
+    companies:[candidateExperience]
+}
+
+input candidateExperienceInput{
+    company_name: String!
+    role:String!
+}
+
+input addCandidateExperienceInput{
+    candidate_id: Int!
+    tags:[String]
+    companies:[candidateExperienceInput]
 }
 
 type Query {
     helloworld: String
-    retrieveCandidateExperience(candidate_id:Int!):candidatesExperienceWithTags
+    retrieveCandidateExperience(candidate_id:Int!):candidateExperienceWithTags
+}
+type Mutation{
+    addCandidateExperience(input:addCandidateExperienceInput): Boolean
+
 }
 
-`
+`;
